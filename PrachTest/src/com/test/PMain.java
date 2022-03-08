667004bb374227752bf62a0b010a9e9a34498959
Prachi Panchal <prachipanchal353@gmail.com>
2022-03-08 16:29:16 +0530

    test project commit

---
+++ PrachTest/src/com/test/PMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author prachi
+ *
+ */
+public class PMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TesT Print!!");
+	}
+
+}
